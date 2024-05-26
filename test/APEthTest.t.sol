d2c4df6c2d9ca64bf617eea69db5392c22400943
0xwildhare <sschell1979@gmail.com>
2024-05-25 22:07:23 -0700

    updated test keys

--- test/APEthTest.t.sol
+++ test/APEthTest.t.sol
@@ -30,25 +30,25 @@ contract APETHTest is Test {
     address bob;
 
     //set bool to "true" when fresh keys are added, set to "false" to kill "reconstructed DepositData does not match supplied deposit_data_root"
-    bool workingKeys = false;
+    bool workingKeys = true;
 
     bytes _pubKey =
         hex"aed26c6b7e0e2cc2efeae9c96611c3de6b982610e3be4bda9ac26fe8aea53276201b3e45dbc242bb24af7fb10fc12196";
     bytes _signature =
-        hex"ae0bbe6dde4748513abd90cca11971afc0403f073af9281eeb392e2b18efc52d1ec195c59ed33ba2424c9f6b91eac0d9036b9d03a15c4316ff0f68e8813ccb7acb6475299d4eae5867bca8ce4661086ae4589be9692e9f16d2f817b21a3d9b0e";
-    bytes32 _deposit_data_root = 0x6f9345fcd8d567f357284bcfc1cd9c415f8fc67a7da60edbd414b0fa3324488f;
+        hex"896a621b4e4fad8d14108111c45141370572882362f40b4a6c4c9bce3f1b13c9e59774a270198448b4e535638916d71d0edbe18a74bc9b087972dbf60c14ef214f77fac423848d75f377ae1214cc0dcb09e2b1f3d2da60e74ac9540d8c6b62c7";
+    bytes32 _deposit_data_root = 0x955de0af387a6cc447bb8f2b5554c51245e2239426c7a251381f3175bf99769a;
 
     bytes _pubKey2 =
         hex"91bebd77cd834b056ff242331dfcd3baecf3b89fcba6d866860a7ace128fb204af9b892cc84dd2d4eb933f6f8d0499b1";
     bytes _signature2 =
-        hex"833b82f87c60504cc71cae165d383e4ba0c8c931f7da9ccff13ed56f4fc3652fff822c1100d443ca645cc57bf409f0e81956a2f0e5d87c443309eca42cdac71c120905bb5ad0fd7f3937102c21a1d6286dc659b72e70478d935fcd56cb4ad16a";
-    bytes32 _deposit_data_root2 = 0x647db1f39668a1128aab6ca8c30457228e2ebb43cb7c84097c337e01db444cf9;
+        hex"b00761c3426f8df64b5f048edc547b6478e947261ca9a9fbd88afdbbb7463f430fbf46a276d392c39113bd05b034f21905958415f561ca842a3b094a52d8d5651c8866b6ac4a6a37b3ac1fd5bcc1a483966127a30e41eafed621d9aa48734553";
+    bytes32 _deposit_data_root2 = 0x657fe2c0d7a9ea7cc56cdad00edd249f4bf7a9f48f845b4f2d4bbcd5f241ca46;
 
     bytes _pubKey3 =
         hex"b6ee6088e5b1dca8a7013f702140ab1f4825d349b20f8c4ba8436af36814dfb3309c13d7423898f60c5e332655a54f17";
     bytes _signature3 =
-        hex"b078320729887f5370ca70c92ae91f4a3e74f392ff79c535f1db29b45d1b9ce8b90abf2ecd01b8b554d3c6e3385fca35087df3d60913f7245cf1bea42421b1d724df0be87e3d87779a250b9c520349858dc0b8a93b3b54569ea9da4ec153f492";
-    bytes32 _deposit_data_root3 = 0x22bbf8648a5fb4b6a048b598f27d5015c2a2073ca2e740e70cbeb436ac7bb305;
+        hex"96dcbfd6aa228d56f81cc2ebb0145db72c12a295d531c37f955bb1ab6dfcd0f8fe8ecb0f134cd026fa32520d0fd6ef100e375f697eb0ee441d47a54cdd0a5fcd5598eab5f8d35fd8fdd812a6f3897a8dd972183f1b5a5fc9aa000051e6106376";
+    bytes32 _deposit_data_root3 = 0x02d2bd6e36896af49cebf39e52e6a0ae926dfaa8dfbd70cad4188e3aa12e3bae;
 
     // Set up the test environment before running tests
     function setUp() public {
