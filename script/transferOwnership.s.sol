bc2a6a67d0ac4c1f529ecc177abce8ffbe50a4f8
0xwildhare <sschell1979@gmail.com>
2024-05-26 08:59:49 -0700

    improved transfer script

--- script/transferOwnership.s.sol
+++ script/transferOwnership.s.sol
@@ -16,20 +16,25 @@ contract transferOwnership is ScriptBase {
             vm.startBroadcast();
             _APEth.transferOwnership(newOwner);
             vm.stopBroadcast();
-        }
         console.log("APETH Address:");
         console.logAddress(address(_APEth));
         console.log("newOwner:");
+        } else {
+            console.log("owner is set already");
+        }
         console.logAddress(newOwner);
         if (newOwner != currentGuardian) {
             console.log("***Transfering guardianship***");
             vm.startBroadcast();
             _storageContract.setGuardian(newOwner);
             vm.stopBroadcast();
-        }
         console.log("Storage Contract Address:");
         console.logAddress(address(_storageContract));
-        console.log("newGuardian:");
+        console.log("newGuardian");
+        console.log('\x1b[31m%s\x1b[0m', "(must be confirmed by newGuardian address)");
+        } else {
+            console.log("guardian is already");
+        }
         console.logAddress(newOwner);
     }
 }
