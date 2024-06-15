6748fb1d0dc990611edb3f953f90dd95ecd18948
0xwildhare <sschell1979@gmail.com>
2024-06-15 11:15:14 -0700

    bulk early mint is the default

--- src/APEthEarlyDeposits.sol
+++ src/APEthEarlyDeposits.sol
@@ -71,11 +71,11 @@ contract APEthEarlyDeposits is Ownable{
 
     function mintAPEthBulk(address[] calldata recipients) external onlyOwner{
         for(uint256 i = 0; i < recipients.length; i++) {
-            mintAPEth(recipients[i]);
+            _mintAPEth(recipients[i]);
         }
     }
 
-    function mintAPEth(address recipient) public onlyOwner {
+    function _mintAPEth(address recipient) internal {
         uint256 amount = deposits[recipient];
         deposits[recipient] = 0;
         uint256 newCoins =_APETH.mint{value: amount}();
