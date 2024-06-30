cedb807e634fd400344a30e4a5e9361fa0544388
0xwildhare <sschell1979@gmail.com>
2024-06-29 19:00:50 -0700

    added tests for multi-pods

--- test/APEthTest.t.sol
+++ test/APEthTest.t.sol
@@ -407,6 +407,7 @@ contract APETHTest is Test {
     }
 
     function test_DeployPod() public {
+        assertEq(APEth.getPodIndex(), 0);
         vm.expectRevert();
         APEth.getPodAddress(1);
         vm.prank(owner);
@@ -414,8 +415,82 @@ contract APETHTest is Test {
         vm.prank(staker);
         APEth.deployPod();
         (address pod, address wrapper) = APEth.getPodAddress(1);
+        assertEq(APEth.getPodIndex(), 1);
         console.log("pod address: ", pod);
         console.log("wrapper address: ", wrapper);
+    }
+
+    function test_BasicAccountingWithStakingAndFuzzingMultiplePods(uint128 x, uint128 y, uint128 z)
+        public
+        mintAlice(x)
+    {
+        // Send eth to contract to increase balance
+        vm.deal(address(this), y);
+        payable(address(APEth)).transfer(y);
+        uint256 cap = storageContract.getUint(keccak256(abi.encodePacked("cap.Amount")));
+        uint256 balance = uint256(x) + uint256(y);
+        if (uint256(x) > cap) {
+            balance = uint256(y);
+        }
+        assertEq(address(APEth).balance, balance);
+        //check eth per apeth
+        uint256 ethPerAPEth;
+        if (x == 0) {
+            ethPerAPEth = 1 ether;
+        } else {
+            ethPerAPEth = balance * 1 ether / uint256(x);
+        }
+        assertEq(APEth.ethPerAPEth(), ethPerAPEth, "ethPerAPEth not correct");
+        uint256 ethInValidators;
+        if (balance >= 32 ether) {
+            vm.prank(owner);
+            APEth.grantRole(ETH_STAKER, staker);
+            vm.startPrank(staker);
+            APEth.deployPod();
+            if (!workingKeys && block.chainid != 31337) {
+                vm.expectRevert("DepositContract: reconstructed DepositData does not match supplied deposit_data_root");
+            }
+            APEth.stake(1, _pubKey, _signature, _deposit_data_root);
+            vm.stopPrank();
+            ethInValidators += 32 ether;
+        }
+        if (balance >= 64 ether) {
+            vm.startPrank(staker);
+            APEth.deployPod();
+            if (!workingKeys && block.chainid != 31337) {
+                vm.expectRevert("DepositContract: reconstructed DepositData does not match supplied deposit_data_root");
+            }
+            APEth.stake(2, _pubKey2, _signature2, _deposit_data_root2);
+            vm.stopPrank();
+            ethInValidators += 32 ether;
+        }
+        if (balance >= 96 ether) {
+            vm.startPrank(staker);
+            APEth.deployPod();
+            if (!workingKeys && block.chainid != 31337) {
+                vm.expectRevert("DepositContract: reconstructed DepositData does not match supplied deposit_data_root");
+            }
+            APEth.stake(3, _pubKey3, _signature3, _deposit_data_root3);
+            vm.stopPrank();
+            ethInValidators += 32 ether;
+        }
+        uint256 newBalance = balance;
+        if (workingKeys || block.chainid == 31337) newBalance = balance - ethInValidators;
+        assertEq(address(APEth).balance, newBalance, "contract balance does not match calculated");
+        assertEq(APEth.ethPerAPEth(), ethPerAPEth, "ethperAPEth not correct after staking");
+        uint256 amount = uint256(x) + (uint256(z) * 1 ether / ethPerAPEth);
+        assertEq(APEth.totalSupply(), uint256(x));
+        vm.prank(owner);
+        APEth.grantRole(EARLY_ACCESS, bob);
+        if (amount > cap) vm.expectRevert(); //APETH__CAP_REACHED()
+        hoax(bob);
+        // Mint z eth of tokens and assert the balance
+        APEth.mint{value: z}();
+        uint256 expected = _calculateAmountLessFee(uint256(z) * 1 ether / ethPerAPEth);
+        if (amount <= cap) {
+            assertEq(APEth.balanceOf(bob), expected);
+            assertEq(address(APEth).balance, newBalance + z);
+        }
     }
 
     function test_Revert_DeployPod_notStaker() public {
