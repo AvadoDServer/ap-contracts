064a5d2d05344d3deb808d94c9e6d8b5968c35bc
sponnet <stefaan@ponnet.com>
2024-09-10 15:48:23 +0200

    earlydeposit deploy script updated with sign address

--- script/deployEarlyDepositOnly.s.sol
+++ script/deployEarlyDepositOnly.s.sol
@@ -1,14 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {ScriptBase, ERC1967Proxy, APETH, APEthEarlyDeposits} from "./scriptBase.s.sol";
+import {console} from "forge-std/console.sol";
+import {
+    ScriptBase,
+    APEthEarlyDeposits,
+    APETH,
+    console,
+    Create2,
+    ERC1967Proxy,
+    Upgrades,
+    stdJson,
+    ProxyConfig
+} from "./scriptBase.s.sol";
+import {HelperConfig, NetworkConfig} from "./HelperConfig.s.sol";
 
 contract DeployEarlyDeposits is ScriptBase {
     function run(address owner, address earlydeposit_signer) public returns (APEthEarlyDeposits) {
-        return deployEarlyDeposit(owner,earlydeposit_signer);
+        APEthEarlyDeposits ed = deployEarlyDeposit(owner, earlydeposit_signer);
+        console.logString("address");
+        console.logAddress(address(ed));
+        return ed;
     }
 
-    // function run() public returns (APEthEarlyDeposits) {
-    //     return run(address(0));
-    // }
+    function run() public returns (APEthEarlyDeposits) {
+        return run(address(0), address(0));
+    }
 }
