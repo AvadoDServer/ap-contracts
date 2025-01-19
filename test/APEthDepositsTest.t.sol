9337e56d2418694f4aa036706abdc28c9398b6a5
0xwildhare <0xwildhare@gmail.com>
2025-01-19 11:37:13 -0800

    add: tests for deposit contract

--- test/APEthDepositsTest.t.sol
+++ test/APEthDepositsTest.t.sol
@@ -14,6 +14,12 @@ import {
     console
 } from "./APEthTestSetup.t.sol";
 import {APETH__CONTRACT_LOCKED} from "../src/APETHV2.sol";
+import {
+    APETH_DEPOSITS__SENDER_MUST_BE_DEPOSITOR,
+    APETH_DEPOSITS__DEPOSIT_WITHDRAWN,
+    APETH_DEPOSITS__NUMBER_OF_DEPOSITS_TOO_HIGH,
+    APETH_DEPOSITS__LESS_THAN_MINIMUM_DEPOSIT
+} from "../src/APEthDeposits.sol";
 
 contract APEthDepositsTest is APEthTestSetup {
     function test_mintAPEthBulk() public mintAlice(30 ether) mintBob(2 ether) {
@@ -55,11 +61,60 @@ contract APEthDepositsTest is APEthTestSetup {
         // deposit into APEthDeposits
         vm.prank(charlie);
         apEthDeposits.deposit{value: 10 ether}();
+        hoax(david);
+        apEthDeposits.deposit{value: 5 ether}();
         vm.deal(address(APEth), 98 ether);
         vm.prank(staker);
-        APEth.setWithdrawalTickets(3, ticBoth, 1);
+        APEth.setWithdrawalTickets(3, ticBoth, 2);
         assertApproxEqAbs(9 ether, APEth.balanceOf(charlie), 1 ether, "charlie's apeth balance");
+        assertApproxEqAbs(APEth.balanceOf(charlie) / 2, APEth.balanceOf(david), 1, "david's apeth balance");
         console.log("eth per apeth (after setWithdrawalTickets)", APEth.ethPerAPEth());
         console.log("charlie's apeth balance", APEth.balanceOf(charlie));
+        console.log("david's apeth balance", APEth.balanceOf(david));
+    }
+
+    function test_withdrawal() public {
+        hoax(charlie);
+        apEthDeposits.deposit{value: 1 ether}();
+        uint256 charlieBalance = charlie.balance;
+        vm.prank(charlie);
+        apEthDeposits.withdraw(1);
+        assertEq(charlie.balance, charlieBalance + 1 ether);
+    }
+
+    function test_revert_mintAPEthBulk_wrongCaller() public {
+        hoax(alice);
+        vm.expectRevert(); //accesscontrol
+        apEthDeposits.mintAPEthBulk(1);
+    }
+
+    function test_revert_withdrawal_wrongCaller() public {
+        hoax(alice);
+        apEthDeposits.deposit{value: 1 ether}();
+        vm.expectRevert(APETH_DEPOSITS__SENDER_MUST_BE_DEPOSITOR.selector);
+        vm.prank(bob);
+        apEthDeposits.withdraw(1);
+    }
+
+    function test_revert_withdrawal_alreadyWithdrawn() public {
+        hoax(alice);
+        apEthDeposits.deposit{value: 1 ether}();
+        vm.prank(alice);
+        apEthDeposits.withdraw(1);
+        vm.expectRevert(APETH_DEPOSITS__DEPOSIT_WITHDRAWN.selector);
+        vm.prank(alice);
+        apEthDeposits.withdraw(1);
+    }
+
+    function test_revert_mintAPEthBulk_wrongNumberOfDeposits() public {
+        hoax(address(APEth));
+        vm.expectRevert(APETH_DEPOSITS__NUMBER_OF_DEPOSITS_TOO_HIGH.selector);
+        apEthDeposits.mintAPEthBulk(1);
+    }
+
+    function test_revert_deposit_lessThanMinDeposit() public {
+        hoax(alice);
+        vm.expectRevert(APETH_DEPOSITS__LESS_THAN_MINIMUM_DEPOSIT.selector);
+        apEthDeposits.deposit{value: 1}();
     }
 }
